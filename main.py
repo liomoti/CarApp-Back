15d9a4f6a1f1dd7c9d39e9edae7cdd1840527079
Moti Shaul <moti_shaul@bmc.com>
2023-12-26 11:28:09 +0200

    initial

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Allow CORS for all origins to enable frontend access
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Item(BaseModel):
+    name: str
+    description: str
+    model: str
+    isBlack: bool
+
+@app.post("/items/")
+async def create_item(item: Item):
+    color_item = "Black" if item.isBlack else "White"
+    return f"Hello {item.name}, We received your order for {color_item} {item.model} with {item.description}."
+
